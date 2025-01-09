3022e1155495af33f164d67cc3dda8e5e3c80340
Luc Wallace <144689311+luc-wallace@users.noreply.github.com>
2025-01-09 23:45:38 +0000

    Comment Codegen.hs

--- src/Codegen.hs
+++ src/Codegen.hs
@@ -21,16 +21,19 @@ import qualified LLVM.IRBuilder.Module as L
 import qualified LLVM.IRBuilder.Monad as L
 import Sast
 
+-- keeps track of program state while traversing it
 data Env = Env
   { operands :: Map Identifier AST.Operand,
     breakLabel :: AST.Name,
     continueLabel :: AST.Name
   }
 
+-- type aliases to reduce type repetition
 type LLVM = L.ModuleBuilderT (State Env)
 
 type Codegen = L.IRBuilderT LLVM
 
+-- converts a cluck AST type to an LLVM type
 convType :: MonadState Env m => Type -> m AST.Type
 convType (Pointer Void) = pure $ AST.ptr AST.i8
 convType t = case t of
@@ -43,6 +46,15 @@ convType t = case t of
   Array ty (Just size) -> AST.ArrayType (fromIntegral size) <$> convType ty
   Array ty Nothing -> AST.ArrayType 0 <$> convType ty
 
+genConstant :: SExpr -> C.Constant
+genConstant e = case e of
+  (_, SIntLiteral n) -> C.Int 32 $ fromIntegral n
+  (_, SFloatLiteral d) -> C.Float $ F.Double d
+  (_, SBoolLiteral b) -> C.Int 1 $ if b then 1 else 0
+  (_, SCharLiteral c) -> C.Int 8 $ (fromIntegral . fromEnum) c
+  _ -> error "error: semant failed"
+
+-- calculates size of a type in bytes (sizeof operator)
 sizeof :: Type -> Int
 sizeof t = case t of
   Char -> 1
@@ -54,27 +66,31 @@ sizeof t = case t of
   Array t' (Just size) -> size * sizeof t'
   _ -> error "error: semant failed"
 
+-- declares an external function in the LLVM IR
 emitBuiltIn :: (Identifier, [AST.Type], AST.Type) -> LLVM ()
 emitBuiltIn (name, args, rett) = do
   func <- L.extern ((AST.mkName . unpack) name) args rett
   modify $ \env -> env {operands = M.insert name func (operands env)}
 
+-- builds an LLVM module from the AST definitions
 codegenProgram :: SProgram -> AST.Module
 codegenProgram (SProgram decls) =
   flip evalState (Env {operands = M.empty, breakLabel = "", continueLabel = ""}) $
     L.buildModuleT "cluck" $ do
       mapM_
         emitBuiltIn
-        [ ("malloc", [AST.i32], AST.ptr AST.i8),
+        [ ("malloc", [AST.i32], AST.ptr AST.i8), -- list of built-in functions aside from printf and scanf
           ("free", [AST.ptr AST.i8], AST.void),
           ("sqrt", [AST.double], AST.double),
           ("pow", [AST.double, AST.double], AST.double)
         ]
+      -- prinf and scanf are emitted separately as they use varargs
       printf <- L.externVarArgs (AST.mkName "printf") [AST.ptr AST.i8] AST.i32
       scanf <- L.externVarArgs (AST.mkName "scanf") [AST.ptr AST.i8] AST.i32
       modify $ \env -> env {operands = M.insert "scanf" scanf $ M.insert "printf" printf (operands env)}
       mapM_ codegenDecl decls
 
+-- returns the memory adddress of an LVal, anything on the left-hand side of an assignment expression
 codegenLVal :: LVal -> Codegen AST.Operand
 codegenLVal (LVar ident) = do
   ops <- gets operands
@@ -88,42 +104,39 @@ codegenLVal (LArray ident i) = do
   case M.lookup ident ops of
     Just op -> do
       addr <- L.load op 0
-      L.gep addr [index]
+      L.gep addr [index] -- getelementptr fetches the value at a memory index
     Nothing -> error "error: semant failed"
 
 codegenDecl :: SDecl -> LLVM ()
 codegenDecl (SFunctionDecl t ident args b) = mdo
+  {- mdo is used for recursive definitions, the function needs to be added to the Env before codegen so that
+     the function can call itself recursively -}
   modify $ \env -> env {operands = M.insert ident function (operands env)} -- add memory address to env
   oldState <- get
 
   function <- do
     rett <- convType t
     args' <- mapM mkArg args
-    L.function (AST.mkName $ cs ident) args' rett genBody
+    L.function (AST.mkName $ cs ident) args' rett genBody -- declare function in LLVM IR
   put oldState
   where
     mkArg (t', ident') = (,) <$> convType t' <*> pure (L.ParameterName (BS.toShort (TE.encodeUtf8 ident')))
-    genBody :: [AST.Operand] -> Codegen ()
     genBody ops = do
-      _ <- L.block `L.named` "entry"
+      _ <- L.block `L.named` "entry" -- define an entry block where function execution starts
       mapM_ initParam $ zip args ops
-      codegenStmt b
+      codegenStmt b -- generate code for the function body
       where
+        -- function parameters need to be initialised in the body so that they can be accessed
         initParam ((t', i), op) = do
           ty <- convType t'
-          addr <- L.alloca ty Nothing 0
+          addr <- L.alloca ty Nothing 0 -- allocate memory for the parameter
           L.store addr 0 op
           modify $ \env -> env {operands = M.insert i addr (operands env)}
 codegenDecl (SVariableDecl t ident e) = do
   let name = AST.mkName $ cs ident
       val = case e of
-        Just (_, expr) -> case expr of
-          (SIntLiteral n) -> C.Int 32 $ fromIntegral n
-          (SFloatLiteral d) -> C.Float $ F.Double d
-          (SBoolLiteral b) -> C.Int 1 $ if b then 1 else 0
-          (SCharLiteral c) -> C.Int 8 $ (fromIntegral . fromEnum) c
-          _ -> error "error: semant failed"
-        Nothing -> case t of
+        Just expr -> genConstant expr -- generate constant as global variables can only have constant values
+        Nothing -> case t of -- initalise variable with empty value if it has no value
           Int -> C.Int 0 0
           Float -> C.Float $ F.Double 0
           Bool -> C.Int 0 0
@@ -140,31 +153,33 @@ codegenExpr (_, SFloatLiteral n) = pure $ L.double n
 codegenExpr (_, SBoolLiteral b) = pure $ L.bit $ if b then 1 else 0
 codegenExpr (_, SCharLiteral c) = pure $ L.int8 $ fromIntegral $ fromEnum c
 codegenExpr (_, SStringLiteral s) = do
-  let elems = unpack s ++ "\0"
-  let c = C.Array AST.i8 $ map (C.Int 8 . fromIntegral . fromEnum) (unpack s ++ "\0")
+  let elems = unpack s ++ "\0" -- add null terminator to strings by default
+  let c = C.Array AST.i8 $ map (C.Int 8 . fromIntegral . fromEnum) elems
   ty <- convType (Array Char (Just (length elems)))
-  addr <- L.alloca ty Nothing 0
+  addr <- L.alloca ty Nothing 0 -- allocate array and store string contents
   L.store addr 0 (AST.ConstantOperand c)
-  L.gep addr [L.int64 0, L.int64 0]
+  L.gep addr [L.int64 0, L.int64 0] -- return pointer to first character
 codegenExpr (t, SNull) = do
   ty <- convType t
-  L.inttoptr (L.int64 0) ty
+  L.inttoptr (L.int64 0) ty -- null pointer value
 codegenExpr (_, LVal l) = do
   op <- codegenLVal l
-  L.load op 0
+  L.load op 0 -- load value at LVal memory address
 codegenExpr (_, SFunctionExpr ident args) = do
   op <- gets (fromJust . M.lookup ident . operands)
   args' <- mapM codegenExpr args
   L.call op $ map (,[]) args'
 codegenExpr (_, SAssign lex rex) = do
+  -- store rhs at the address of lhs
   lhs <- codegenLVal lex
   rhs <- codegenExpr rex
   L.store lhs 0 rhs
   return rhs
-codegenExpr (_, SRef l) = codegenLVal l
+codegenExpr (_, SRef l) = codegenLVal l -- return memory address of value
 codegenExpr (_, SBinaryOp op lex rex) = do
   lhs <- codegenExpr lex
   rhs <- codegenExpr rex
+  -- pair up types with corresponding LLVM instruction
   case op of
     Add -> case (fst lex, fst rex) of
       (Int, Int) -> L.add lhs rhs
@@ -238,13 +253,15 @@ codegenExpr (t, SUnaryOp op ex) = do
     Not -> L.xor ex' (L.bit 1)
     _ -> error "internal error: semant failed"
 codegenExpr (t, SInc lval) = do
+  -- load value at LVal
   addr <- codegenLVal lval
   op <- L.load addr 0
+  -- increase value by 1 and store it at memory location
   after <- case t of
     Float -> L.add op (L.double 1)
     _ -> L.add op (L.int32 1)
   L.store addr 0 after
-  return op
+  return op -- return original value
 codegenExpr (t, SDec lval) = do
   addr <- codegenLVal lval
   op <- L.load addr 0
@@ -278,12 +295,11 @@ codegenStmt (SBlockStmt stmts) = do
   let (l, r) = break isReturn stmts -- filter out unreachable statements after return
   mapM_ codegenStmt (l ++ take 1 r)
   where
-    isReturn :: SStmt -> Bool
     isReturn (SReturnStmt _) = True
     isReturn _ = False
 codegenStmt (SIfStmt cond t e) = mdo
   c <- codegenExpr cond
-  L.condBr c thenBlock elseBlock
+  L.condBr c thenBlock elseBlock -- goes to thenBlock if condition is true else elseBlock
 
   thenBlock <- L.block `L.named` "then"
   codegenStmt t
@@ -293,7 +309,7 @@ codegenStmt (SIfStmt cond t e) = mdo
   codegenStmt e
   mkTerminator $ L.br mergeBlock
 
-  mergeBlock <- L.block `L.named` "merge"
+  mergeBlock <- L.block `L.named` "merge" -- both paths merge after condition
   pure ()
 codegenStmt (SSwitchStmt e cs') = mdo
   cond <- codegenExpr e
@@ -302,20 +318,15 @@ codegenStmt (SSwitchStmt e cs') = mdo
   epilogBlock <- L.block `L.named` "epilog"
   pure ()
   where
-    genCase epilog (SSwitchCase (_, val) stmts) = do
+    genCase epilog (SSwitchCase val stmts) = do
       l <- L.block `L.named` "case"
       mapM_ codegenStmt stmts
       mkTerminator $ L.br epilog
-      let c = case val of
-            SIntLiteral n -> C.Int 32 (fromIntegral n)
-            SCharLiteral n -> C.Int 8 $ (fromIntegral . fromEnum) n
-            SFloatLiteral f -> C.Float $ F.Double f
-            SBoolLiteral b -> if b then C.Int 1 1 else C.Int 1 0
-            _ -> error "error: semant failed"
-      pure (c, l)
+      pure (genConstant val, l)
 codegenStmt (SDoWhileStmt body cond) = mdo
   L.br bodyBlock
   oldEnv <- get
+  -- add labels to Env so break and continue point to the right label
   modify $ \env -> env {breakLabel = endBlock, continueLabel = bodyBlock}
 
   bodyBlock <- L.block `L.named` "body"
@@ -338,6 +349,7 @@ codegenStmt (SForStmt init' cond inc body) = mdo
   c <- codegenExpr cond
   mkTerminator $ L.condBr c incBlock endBlock
 
+  -- for loops have a separate inc block to ensure that the loop increments when a continue statement is run 
   incBlock <- L.block `L.named` "inc"
   codegenStmt inc
   L.br bodyBlock
@@ -346,8 +358,8 @@ codegenStmt (SForStmt init' cond inc body) = mdo
   modify $ const oldEnv
 codegenStmt (SVariableDeclStmt t ident expr) = do
   ty <- convType t
-  addr <- L.alloca ty Nothing 0
-  modify $ \env -> env {operands = M.insert ident addr (operands env)} -- add memory address to env
+  addr <- L.alloca ty Nothing 0 -- allocate memory for variable
+  modify $ \env -> env {operands = M.insert ident addr (operands env)}
   case expr of
     Nothing -> pure ()
     Just e -> do
@@ -359,7 +371,7 @@ codegenStmt (SArrayDeclStmt t ident size items) = do
   decayTy <- convType (Pointer t)
 
   decayPtr <- L.alloca decayTy Nothing 0 -- init decay pointer
-  decayVal <- L.gep addr [L.int64 0, L.int64 0]
+  decayVal <- L.gep addr [L.int64 0, L.int64 0] -- return pointer to first item
   L.store decayPtr 0 decayVal
 
   modify $ \env -> env {operands = M.insert ident decayPtr (operands env)}
@@ -381,6 +393,7 @@ codegenStmt SContinueStmt = do
   cont <- gets continueLabel
   L.br cont
 
+-- checks if a block has a terminator before putting an instruction
 mkTerminator :: Codegen () -> Codegen ()
 mkTerminator instr = do
   check <- L.hasTerminator
